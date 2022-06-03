69cd7ca4ab93a9e5768715cd7dc96d57c78da7a9
Bayori <supercatura@gmail.com>
2022-06-03 15:22:34 +0500

    Apex Legends RPC

---
+++ Constants/Translation.js
@@ -0,0 +1,42 @@
+module.exports = Object.freeze({
+    // Manual Firing Range
+    "#PL_FIRINGRANGE-ALONE": "На стрельбище, в одиночку",
+    "#PL_FIRINGRANGE-PARTY": "На стрельбище, с друзьями",
+
+    // Lobby
+    "#RICHPRESENCE_PARTY_LOBBY_N_N": "Кайфует в лобби 🤤",
+    "#RICHPRESENCE_PARTY_LOBBY": "Кайфует в лобби 🤤",
+
+    // Game Mode
+    "#PL_TRIO": "ТРИО",
+    "#PL_DUO": "ДУО",
+    "#PL_Ranked_Leagues": "РЕЙТИНГ",
+    "#GAMEMODE_ARENAS": "АРЕНЫ",
+    "#GAMEMODE_ARENAS_RANKED": "РЕЙТ. АРЕНЫ",
+    "#GAME_MODE_CONTROL": "КОНТРОЛЬ",
+    "#pl_ranked_leagues": "РЕЙТИНГ",
+    "#PL_TRAINING": "Тренируется",
+    "#LOADINGSCREEN": "Загружается в катку...",
+    "#MAINMENU": "В главном меню",
+
+    // Map Level
+    // Event Map
+    "#mp_rr_canyonlands_mu1_night": "Битва Теней",
+    "#mp_rr_canyonlands_mu1_night_desc": "Битва Теней",
+    "#mp_rr_desertlands_holiday": "Зимний Экспресс",
+    "#mp_rr_desertlands_holiday_desc": "Зимний Экспресс",
+
+    // Normal Map
+    "#mp_rr_canyonlands": "Каньон Кингс",
+    "#mp_rr_desertlands": "Край Света",
+    "#mp_rr_olympus": "Олимп",
+    "#mp_rr_tropic_island": "Место бури",
+
+    // Arena Map
+    "#mp_rr_arena_phase_runner": "Фазовый переход",
+    "#mp_rr_party_crasher": "Внезапная вечеринка",
+    "#mp_rr_aqueduct": "Переполнение",
+    "#mp_rr_arena_skygarden": "Инкор",
+    "#mp_rr_arena_composite": "Высадка",
+    "#mp_rr_arena_habitat": "Среда обитания 4"
+});
